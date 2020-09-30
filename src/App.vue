25928a767d3ed7211ea15560ccd031400883a3e1
yxb <yuxiaobing1997@163.com>
2020-09-30 11:29:13 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   <!-- 路由的出口 -->
   <router-view />
   <h1>哈哈哈</h1>
+  <div><i class="toutiao toutiao-shipin"></i></div>
+  <van-button type="primary">主要按钮</van-button>
 </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入 vant 组件
+import Vant from 'vant'
+// 引入 vant 的样式文件
+import 'vant/lib/index.css'
+
+// 注册 vant
+Vue.use(Vant)
+
 // 加载全局样式
 import './styles/global.css'
 
